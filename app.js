354c128c302e6a78f137570b82f1ee992dd28f67
Daniel Flores D <daniel.flor3s.d@gmail.com>
2023-01-31 14:30:14 -0700

    README

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const routes = require("./routes");
+const path = require("path");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+
 app.use(routes);
+
+
+
+
 
 module.exports = app;
